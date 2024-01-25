400c611d14559e195c898979aa191349c728782a
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-25 15:45:30 +0100

    fix: fix tailwind style for main in RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-dvh">
       <header className="bg-amber-400">Header</header>
-      <main className="flex-grow">
+      <main className="grow">
         <Outlet />
       </main>
       <footer className="bg-amber-400">Footer</footer>
