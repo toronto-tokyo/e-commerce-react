3e3ea8dc259b457cd8a1d4c57b7fca5ffbbefded
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-31 19:44:10 +0100

    feat: add link to navigate to the sign up page at SignInPage

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import SignInForm from 'components/SingInForm';
+import { Link } from 'react-router-dom';
+import Divider from 'components/Divider';
 
 const SignInPage: React.FC = () => {
   return (
     <div className="grow flex justify-center items-center px-3 py-10">
-      <section className="bg-white p-5 rounded-md basis-3/5 min-w-72 max-w-2xl flex flex-col">
+      <section className="bg-white p-5 rounded-md basis-3/5 min-w-72 max-w-2xl flex flex-col gap-5">
         <SignInForm />
+        <Divider />
+        <p className="flex flex-wrap items-center justify-center gap-1">
+          <span>Don&apos;t have an account?</span>
+          <Link to="/sign-up" className="text-blue-500 font-bold">
+            Sign up here!
+          </Link>
+        </p>
       </section>
     </div>
   );
