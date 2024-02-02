1abde9221b137fae1786477e15ca567f1bd790d1
Minchuk Ilya <cacatubutyt@gmail.com>
2024-02-02 12:35:43 +0100

    feat: add a button or link on the registration page that allows users to navigate to the login page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,5 +1,7 @@
+import Divider from 'components/Divider';
 import SignUpForm from 'components/SignUpForm';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
   return (
@@ -7,6 +9,13 @@ const SignUpPage = () => {
       <section className="bg-white p-5 rounded-md basis-3/5 flex flex-col">
         <h1 className="self-center">Sign In</h1>
         <SignUpForm />
+        <Divider />
+        <p className="flex flex-wrap items-center justify-center gap-1">
+          <span>Already registered?</span>
+          <Link to="/sign-in" className="text-blue-500 font-bold">
+            Sign in here!
+          </Link>
+        </p>
       </section>
     </div>
   );
