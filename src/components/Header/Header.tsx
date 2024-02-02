9757504ce6a3f7129e3898459bc07bc8c50802c4
Minchuk Ilya <cacatubutyt@gmail.com>
2024-02-02 17:56:54 +0100

    feat: add ability to access the main page for all users via navigation button or logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,20 @@ import { MdLogin } from 'react-icons/md';
 import { FaRegUser } from 'react-icons/fa';
 import NavListItem from 'components/NavListItem';
 import Navigation from 'components/Navigation';
+import { AiOutlineHome } from 'react-icons/ai';
+import Logo from 'components/Logo';
 
 const Header: React.FC = () => {
   const { isLoggedIn } = useAuth();
   return (
     <header className="bg-indigo-900">
       <div className="flex justify-between max-w-7xl min-h-14 px-3 m-auto">
-        <p>logo</p>
+        <Logo />
         <Navigation>
+          <NavListItem to="/">
+            <AiOutlineHome className="text-red-500" />
+            <span>Home</span>
+          </NavListItem>
           {!isLoggedIn && (
             <>
               <NavListItem to="sign-in">
