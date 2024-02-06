d580c9b4229bd5a68e4d8c330d61ff32cccc9bae
Minchuk Ilya <cacatubutyt@gmail.com>
2024-02-06 13:41:39 +0100

    refactor: remove filterSlice from redux store

--- src/components/ProductFilters/ProductFilters.tsx
+++ src/components/ProductFilters/ProductFilters.tsx
@@ -1,82 +1,107 @@
 import Collapsible from 'components/Collapsible';
 import useGetAttributes from 'hook/useGetAttributes';
-import { useAppDispatch, useAppSelector } from 'hook/useRedux';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { setFilters, toggleBrand, toggleColor } from 'store/slice/filterSlice';
 
 const ProductFilters: React.FC = () => {
   const data = useGetAttributes();
-  const dispatch = useAppDispatch();
   const brands = data?.find((attribute) => attribute.name === 'designer');
   const colors = data?.find((attribute) => attribute.name === 'color');
-  const { brands: filterBrands, colors: filterColors } = useAppSelector(
-    (store) => store.filterSlice
-  );
   const [searchParams, setSearchParams] = useSearchParams();
+  const searchBrands = searchParams.get('brands')?.split(',') || [];
+  const searchColors = searchParams.get('colors')?.split(',') || [];
 
-  useEffect(() => {
-    const brands = searchParams.get('brands');
-    const colors = searchParams.get('colors');
-    const filters = {
-      brands: brands?.split(',') || [],
-      colors: colors?.split(',') || [],
-    };
-    dispatch(setFilters(filters));
-  }, [searchParams, dispatch]);
+  const handleMultipleFilterClick = ({
+    filterTitle,
+    filterValue,
+    currentFilterValues,
+  }: {
+    filterTitle: string;
+    filterValue: string;
+    currentFilterValues: string[];
+  }) => {
+    if (currentFilterValues.includes(filterValue)) {
+      currentFilterValues = currentFilterValues.filter(
+        (item) => item !== filterValue
+      );
+    } else {
+      currentFilterValues.push(filterValue);
+    }
+    currentFilterValues.length
+      ? searchParams.set(filterTitle, currentFilterValues.join(','))
+      : searchParams.delete(filterTitle);
+
+    setSearchParams(searchParams);
+  };
+
+  const handleBrandFilterClick = (filterValue: string) => {
+    handleMultipleFilterClick({
+      filterTitle: 'brands',
+      filterValue,
+      currentFilterValues: searchBrands,
+    });
+  };
+
+  const handleColorsFilterClick = (filterValue: string) => {
+    handleMultipleFilterClick({
+      filterTitle: 'colors',
+      filterValue,
+      currentFilterValues: searchColors,
+    });
+  };
 
   return (
     <div className="w-[250px] bg-white">
       <Collapsible label="Brand" maxHeight={150}>
-        {brands?.type.name === 'enum' &&
-          brands?.type.values.map((brand) => (
-            <div key={brand.key}>
-              <input
-                type="checkbox"
-                name="brand"
-                value={brand.key}
-                id={brand.key}
-                checked={filterBrands.includes(brand.key)}
-                onChange={(e) => dispatch(toggleBrand(e.target.value))}
-              />
-              <label htmlFor={brand.key} className="select-none">
-                {brand.label}
-              </label>
-            </div>
-          ))}
+        <div
+          onChange={(e) => {
+            const target = e.target as HTMLInputElement;
+            const value = target.value;
+            handleBrandFilterClick(value);
+          }}
+        >
+          {brands?.type.name === 'enum' &&
+            brands?.type.values.map((brand) => (
+              <div key={brand.key}>
+                <input
+                  type="checkbox"
+                  name="brand"
+                  value={brand.key}
+                  id={brand.key}
+                  defaultChecked={searchBrands?.includes(brand.key)}
+                />
+                <label htmlFor={brand.key} className="select-none">
+                  {brand.label}
+                </label>
+              </div>
+            ))}
+        </div>
       </Collapsible>
       <Collapsible label="Colors" maxHeight={150}>
-        {colors?.type.name === 'lenum' &&
-          colors?.type.values.map((color) => (
-            <div key={color.key}>
-              <input
-                type="checkbox"
-                name="color"
-                value={color.key}
-                id={color.key}
-                checked={filterColors.includes(color.key)}
-                onChange={(e) => dispatch(toggleColor(e.target.value))}
-              />
-              <label htmlFor={color.key} className="select-none">
-                {color.label.en}
-              </label>
-            </div>
-          ))}
+        <div
+          onChange={(e) => {
+            const target = e.target as HTMLInputElement;
+            const value = target.value;
+            handleColorsFilterClick(value);
+          }}
+        >
+          {colors?.type.name === 'lenum' &&
+            colors?.type.values.map((color) => (
+              <div key={color.key}>
+                <input
+                  type="checkbox"
+                  name="color"
+                  value={color.key}
+                  id={color.key}
+                  defaultChecked={searchColors?.includes(color.key)}
+                />
+                <label htmlFor={color.key} className="select-none">
+                  {color.label.en}
+                </label>
+              </div>
+            ))}
+        </div>
       </Collapsible>
-      <button
-        className="p-5 bg-blue-500"
-        onClick={() => {
-          filterBrands.length
-            ? searchParams.set('brands', filterBrands.join(','))
-            : searchParams.delete('brands');
-          filterColors.length
-            ? searchParams.set('colors', filterColors.join(','))
-            : searchParams.delete('colors');
-          setSearchParams(searchParams);
-        }}
-      >
-        apply
-      </button>
     </div>
   );
 };
